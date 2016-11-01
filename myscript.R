e24a9ceef730bff3aa2545d607c2c142107074f4
neethaiyer <neethaiyer@campus-007-111.ucdavis.edu>
2016-11-01 13:59:11 -0700

    added linear model

--- myscript.R
+++ myscript.R
@@ -3,3 +3,12 @@
 x <- rnorm(n=1000, mean=0, sd=3)
 hist(x, nclass=30)
 
+#### linear regression y=mx+b+Ei
+m <- 3
+b <- -2
+eps <- rnorm(1000,0,20)
+
+x <- runif(1000,0,100)
+y <- m*x+b+eps
+hist(x)
+plot(x, y, cex=0.5)
